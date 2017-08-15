5453346fa1d821fc3dcd9f3dcc420497babc940b
Nicolas Ojeda Bar <n.oje.bar@gmail.com>
2017-08-15 08:12:53 +0530

    Fix Makefile, example

--- Makefile
+++ Makefile
@@ -1,29 +1,21 @@
-CAMLFLAGS=
-CAMLC=ocamlc $(CAMLFLAGS)
-CAMLOPT=ocamlopt $(CAMLFLAGS)
+all:
+	jbuilder build @install
 
-test: xapian_stubs.o xapian.cmx test.cmx
-	$(CAMLOPT) -cclib -lstdc++ -cclib '$(shell pkg-config xapian-core --libs)' $^ -o $@
-
-xapian_stubs.o: xapian_stubs.cc
-	$(CC) -I$(shell $(CAMLC) -where) $(shell pkg-config xapian-core --cflags) -c $<
-
-xapian.cmi: xapian.mli
-	$(CAMLC) -c $^
-
-xapian.cmo: xapian.ml xapian.cmi
-	$(CAMLC) -c $<
-
-xapian.cmx: xapian.ml xapian.cmi
-	$(CAMLOPT) -c $<
-
-test.cmo: test.ml xapian.cmi
-	$(CAMLC) -c $<
-
-test.cmx: test.ml xapian.cmi
-	$(CAMLOPT) -c $<
+examples:
+	jbuilder build examples/csvtest.exe
 
 clean:
-	rm -f *.cm* *.o
+	jbuilder clean
 
-.PHONY: clean
+install:
+	jbuilder install
+
+uninstall:
+	jbuilder uninstall
+
+reinstall: uninstall install
+
+doc:
+	jbuilder build @doc
+
+.PHONY: all clean doc install uninstall
--- examples/csvtest.ml
+++ examples/csvtest.ml
@@ -1,26 +1,31 @@
 open Xapian
 
-let read_csv _ =
-  assert false
-
 let () =
-  let csv = read_csv Sys.argv.(1) in
+  let ic = open_in Sys.argv.(1) in
+  let csv_ic = Csv.of_channel ic in
   let db = WritableDatabase.create "db" in
   let tg = TermGenerator.create () in
   TermGenerator.set_stemmer tg (Stem.create "en");
-  List.iter (fun row ->
-      let doc = Document.create () in
-      TermGenerator.set_document tg doc;
-      TermGenerator.index_text tg ~wdf_inc:1 ~prefix:"S" (List.assoc "TITLE" row);
-      TermGenerator.index_text tg ~wdf_inc:2 ~prefix:"XD" (List.assoc "DESCRIPTION" row);
-
-      TermGenerator.index_text tg (List.assoc "TITLE" row);
-      TermGenerator.increase_termpos tg;
-      TermGenerator.index_text tg (List.assoc "DESCRIPTION" row);
-
-      Document.set_data doc "jjaja";
-
-      let idterm = "Q" ^ List.assoc "id_NUMBER" row in
-      Document.add_boolean_term doc idterm;
-      WritableDatabase.replace_document db idterm doc
-    ) csv
+  let titles = Csv.next csv_ic in
+  let rec loop () =
+    match Csv.next csv_ic with
+    | row ->
+        let row = List.combine titles row in
+        let title = List.assoc "TITLE" row in
+        let description = List.assoc "DESCRIPTION" row in
+        let id_number = List.assoc "id_NUMBER" row in
+        let doc = Document.create () in
+        TermGenerator.set_document tg doc;
+        TermGenerator.index_text tg ~wdf_inc:1 ~prefix:"S" title;
+        TermGenerator.index_text tg ~wdf_inc:2 ~prefix:"XD" description;
+        TermGenerator.index_text tg title;
+        TermGenerator.increase_termpos tg;
+        TermGenerator.index_text tg description;
+        Document.set_data doc "jjaja";
+        let idterm = "Q" ^ id_number in
+        Document.add_boolean_term doc idterm;
+        WritableDatabase.replace_document db idterm doc
+    | exception End_of_file ->
+        close_in ic
+  in
+  loop ()
